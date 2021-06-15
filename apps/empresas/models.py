15d671a9845d32e1aeceba2cf459cabe39e7f695
Gabriel Fernandes <gfernandes.profissional@gmail.com>
2021-06-15 19:16:56 -0300

    Definindo os relacionamentos do BD

--- apps/empresas/models.py
+++ apps/empresas/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
 class Empresa(models.Model):
 	nome = models.CharField(max_length=100, help_text="NOME da EMPRESA")
+
+	def __str__(self):
+		return self.nome
