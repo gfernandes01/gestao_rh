7d3a927ec1c369b5c27077ea8f8b801f2cdb09bf
Gabriel Fernandes <gfernandes.profissional@gmail.com>
2021-07-08 09:33:37 -0300

    Criação listagem Banco de Horas

---
+++ apps/registro_hora_extra/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import HoraExtraList
+
+
+urlpatterns = [
+    path('', HoraExtraList.as_view(), name='list_hora_extra'),
+    # path('editar/<int:pk>/', FuncionariosEdit.as_view(), name='update_funcionarios'),
+    # path('deletar/<int:pk>/', FuncionariosDelete.as_view(), name='delete_funcionarios'),
+    # path('novo/', FuncionariosCreate.as_view(), name='create_funcionarios'),
+]
+# FuncionarioEdit, FuncionarioDelete, FuncionarioNovo
--- apps/registro_hora_extra/views.py
+++ apps/registro_hora_extra/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import registroHoraExtra
+from django.views.generic import ListView
+
 
 # Create your views here.
+class HoraExtraList(ListView):
+	model = registroHoraExtra
+
+	def get_queryset(self):
+		empresa_logada = self.request.user.funcionario.empresa
+		return registroHoraExtra.objects.filter(funcionario__empresa=empresa_logada)
