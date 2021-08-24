5580d6d7e25edfb97182d904314441a0b8f63775
Gabriel Fernandes <gfernandes.profissional@gmail.com>
2021-08-24 14:32:19 -0300

    Implementaçao do DJANGO REST framework

--- gestao_rh/urls.py
+++ gestao_rh/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
+
+from rest_framework import routers
+from apps.core import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     path('', include('apps.core.urls')),
@@ -27,4 +35,7 @@ urlpatterns = [
     path('documentos/', include('apps.documentos.urls')),
     path('departamentos/', include('apps.departamentos.urls')),
     path('horas-extras/', include('apps.registro_hora_extra.urls')),
+
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
