d1c09ac6bd2c597fbba7ba3e3251db2745e78661
Gabriel Fernandes <gfernandes.profissional@gmail.com>
2021-08-24 15:57:10 -0300

    configuração da autenticação e autorização via token

--- apps/registro_hora_extra/api/views.py
+++ apps/registro_hora_extra/api/views.py
@@ -1,8 +1,13 @@
 from rest_framework import viewsets
 from apps.registro_hora_extra.api.serializers import registroHoraExtraSerializer
 from apps.registro_hora_extra.models import registroHoraExtra
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 
 
 class registroHoraExtraViewSet(viewsets.ModelViewSet):
 	queryset = registroHoraExtra.objects.all()
 	serializer_class = registroHoraExtraSerializer
+	authentication_classes = (TokenAuthentication,)
+	permission_classes = (IsAuthenticated,)
+
