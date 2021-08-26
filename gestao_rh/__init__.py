70ff1aa56f571ddef8c99b2d7b01057491ff0927
Gabriel Fernandes <gfernandes.profissional@gmail.com>
2021-08-26 14:46:27 -0300

    Implementando Celery

---
+++ gestao_rh/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import absolute_import, unicode_literals
+from .celery import app as celery_app
+
+
+___all___ = ('celery_app',)
--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -19,6 +19,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_celery_results',
     'rest_framework',
     'rest_framework.authtoken',
     'apps.empresas',
@@ -102,3 +103,10 @@ LOGOUT_REDIRECT_URL = 'login'
 
 # Default primary key field type - https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CELERY_RESULT_BACKEND = 'django-db'
+
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TASK_SERIALIZER = 'json'
